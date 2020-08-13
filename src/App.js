8afe39149797846fa6215f447a9c0fd1aee9d4dc
dirkjf <d.j.faber@outlook.com>
2020-08-13 10:05:53 +0200

    Initial code.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [width, setWidth] = useState(60);
+    const [height, setHeight] = useState(50);
+    const [aliveProbability, setAliveProbability] = useState(40); // Percentage
+
+    const renderGridRow = () => {
+        let gridRow = [];
+        for (let i = 0; i < width; i++) {
+            gridRow[i] = <Cell key={i} aliveProbability={aliveProbability} />;
+        }
+        return gridRow;
+    }
+
+    const renderGrid = () => {
+        let gridRows = [];
+        for (let i = 0; i < height; i++) {
+            gridRows[i] = <Row key={i}>{renderGridRow(i)}</Row>;
+        }
+        return (gridRows);
+    }
+
+    return (
+        <div className="grid">
+            {renderGrid()}
+        </div>
+    );
+}
+
+function Cell(props) {
+
+    const [alive, setAlive] = useState(Math.random() < props.aliveProbability/100);
+
+    useEffect(() => {
+        const interval = setInterval(() => setAlive(Math.random() < props.aliveProbability/100), 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className={"cell " + (alive ? 'alive' : '') }>
+            {props.children}
+        </div>
+    );
+
+
+}
+
+function Row(props) {
+    return (
+        <div className="row">
+            {props.children}
+        </div>
+    );
 }
 
 export default App;
